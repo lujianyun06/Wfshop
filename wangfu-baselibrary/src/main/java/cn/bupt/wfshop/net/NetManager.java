6a8ebb872e5f3fbe5e8415b5bc0eefd36ebce36d
lujianyun <lujianyun66@163.com>
2018-08-14 16:56:59 +0800

    增加NetManager类总管url

---
+++ wangfu-baselibrary/src/main/java/cn/bupt/wfshop/net/NetManager.java
@@ -0,0 +1,14 @@
+package cn.bupt.wfshop.net;
+
+/**
+ * Created by Lujianyun on 2018/8/1.
+ */
+
+public class NetManager {
+    public static final String BASEURL = "http://www.jessicaleech.com";
+//    public static final String BASEURL = "http://wfshop.andysheng.cn";
+    //    public static final String BASEURL = "http://wfshop.andysheng.cn";
+    public static final String CART_URL = BASEURL + "/cart";
+    public static final String ORDER_URL = BASEURL + "/order";
+    public static final String HOME_PAGE = "homepage";
+}
